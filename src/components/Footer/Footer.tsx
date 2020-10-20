47a2a5e2225c0ed3b58b4d1d72ed772334c07339
LabiMurati <labimurati25@gmail.com>
2020-10-20 02:14:50 +0200

    Footer it's done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 //styles
 import './Footer.scss'
-//images
+
 import facebook from './../../assets/images/facebook-footer.png';
 import twitter from './../../assets/images/twitter-footer.png';
 import linkedin from './../../assets/images/004-linkedin-logo.png';
@@ -11,29 +11,48 @@ import youtube from './../../assets/images/youtube-footer.png';
 import icon from './../../assets/images/icon-footer.png';
 
 
-
 export const Footer = () => {
     return (
         <div className="Footer">
-            <section className="block_section footer_section">
-                <div className="container">
-                    <div className="logo">
-                    <img src={icon}  alt="icon"/>
-                    <h2><b>E</b>-Shop</h2>
+            <div className="container">
+                <div className="Footer_wrap">
+                    <div className="Footer__first">
+                        <h2><b>E</b>-Shop</h2>
+                        <img src={icon} alt="" />
+                        <p>House My Brand designs clothing for the young, the old &
+                        everyone in between – but most importantly, for the fashionable</p>
+                        <div className="Footer__icons">
+                            <img src={facebook} alt="fb" />
+                            <img src={twitter} alt="tw" />
+                            <img src={linkedin} alt="ln" />
+                            <img src={instagram} alt="ig" />
+                            <img src={youtube} alt="yt" />
+                        </div>
                     </div>
-                    <div className="footer__left">
-                         <p>House My Brand  designs clothing for the young, the old & everyone in between – but most importantly, for the fashionable</p>
-                    </div>
-                    <div className="footer__icons">
-                    <img src={facebook}  alt="fb"/>
-                    <img src={twitter}  alt="tw"/>
-                     <img src={linkedin} alt="inn"/>
-                    <img src={instagram}  alt="ig"/> 
-                    <img src={youtube}  alt="yt"/>
-                    </div>
+                    <ul>
+                        <h3>Shopping Online</h3>
+                        <li>Order Status</li>
+                        <li>Shipping and Delivery</li>
+                        <li>Returns</li>
+                        <li>Payment Options</li>
+                        <li>Contact Us</li>
+                    </ul>
+                    <ul>
+                        <h3>Information</h3>
+                        <li>Gift Cards</li>
+                        <li>Find a store</li>
+                        <li>NewsLetter</li>
+                        <li>Become a member</li>
+                        <li>Site Feedback</li>
+                    </ul>
+                    <ul>
+                        <h3>Contact</h3>
+                        <li>store@uikit.com</li>
+                        <li>Hotline: 585158</li>
+                    </ul>
                 </div>
-            </section>
-            <p>DESIGN BY ICEO.CO - © 2019. ALL RIGHTS RESERVED.</p>
+            </div>
+            <h5>DESIGN BY ICEO.CO - © 2019. ALL RIGHTS RESERVED.</h5>
         </div>
     )
 }
